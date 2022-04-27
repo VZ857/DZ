20b06d89c32b3900ba398096ddbdcdbbadfc6be5
Vadim <>
2022-04-27 22:38:35 +0700

    Task2

---
+++ dz/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на
+//вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите певрое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите певрое число");
+int num3 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число больше");
+
+}
+else if (num2 >num3)
+
+{
+        Console.WriteLine("Второе число больше");
+
+}
+else 
+{
+        Console.WriteLine("Третье число больше");
+}
+
