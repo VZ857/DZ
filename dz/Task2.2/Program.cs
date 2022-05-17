455984f30be27d8bf2ea0e6979b5e54546972688
Vadim <>
2022-05-17 23:23:04 +0700

    Решение 2й задачи

--- dz/Task2.2/Program.cs
+++ dz/Task2.2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+
+
+Console.WriteLine("Введите число");
+    while(true)
+    {
+int num1 = Convert.ToInt32(Console.ReadLine());  
+string num1Text = Convert.ToString(num1);    
+ if (num1Text.Length > 2)
+            Console.WriteLine("третья цифра - " + num1Text[2]);
+        else
+            Console.WriteLine("третьей цифры нет");
+    }
+
