9532705722c511dbf02127b9f4cfa4ea447f7119
Vadim <>
2022-05-18 23:33:00 +0700

    2.2

--- dz/Task2.2/Program.cs
+++ dz/Task2.2/Program.cs
@@ -6,7 +6,7 @@
 Console.WriteLine("Введите число");
     while(true)
     {
-int num1 = Convert.ToInt32(Console.ReadLine());  
+int num1 = int.Parse(Console.ReadLine());  
 string num1Text = Convert.ToString(num1);    
  if (num1Text.Length > 2)
             Console.WriteLine("третья цифра - " + num1Text[2]);
